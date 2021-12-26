301e959a434797ab55b77420e75ba6287ed18251
BadAr-78 <mohamedbadar218@gmail.com>
2021-12-27 00:55:21 +0200

    update sliders

--- index.php
+++ index.php
@@ -8,17 +8,17 @@
         $sliders = array(
         '1.jpg',
         '2.jpg',
-        // '3.jpg',
+        '3.jpg',
         '4.jpg',
-        // '5.jpg',
+        '5.jpg',
         '6.jpg',
         '7.jpg',
         '8.jpg',
         '9.jpg',
         '10.jpg',
-        '11.jpg',
-        '12.jpg',
-        '13.jpg',
+        // '11.jpg',
+        // '12.jpg',
+        // '13.jpg',
         );
         foreach($sliders as $slider){
           ?>
